fa63441df17e09870d4e87a871332f648881ea40
Wellington Felix <wfmf@cin.ufpe.br>
2018-03-17 13:51:38 -0300

    Questao 2

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,19 @@ import Questao2
 
 main :: IO ()
 main = do
-  let str = "kdvnhoo lv ixq"
-  return ()
-  -- putStrLn $ crack str
+  let str1 = "haskell is a lot of fun"
+  let str2 = "for writers a random sentence can help them get their creative juices flowing since the topic of the sentence is completely unknown it forces the writer to be creative when the sentence appears there are a number of different ways a writer can use the random sentence for creativity the most common way to use the sentence is to begin a story another option is to include it somewhere in the story a much more difficult challenge is to use it to end a story in any of these cases it forces the writer to think creatively since they have no idea what sentence will appear from the tool"
+  let test2 = (decode 3 . encode 3 $ str2) == (crack str2)
+  let test1 = (decode 3 . encode 3 $ str1) == (crack str1)
+  putStrLn . show $ test1
+  putStrLn . show $ test2
+  putStrLn "isTaut"
+  let a = Var 'A'
+  let b = Var 'B'
+  let c = Var 'C'
+  let prop = ((a :->: b) :&: (b :->: c)) :->: (a :->: c)
+  let prop2 = (a :&: b) :->: a
+  putStr "((A -> B) ^ (B -> C) -> (A -> C)) é tautologia? "
+  putStrLn . show $ isTaut prop
+  putStr "(A ^ B) -> A é tautologia? "
+  putStrLn . show $ isTaut prop2
--- src/questao2/Questao2.hs
+++ src/questao2/Questao2.hs
@@ -1,9 +1,73 @@
 module Questao2 where
 
+import qualified Data.Set as S
 
--- | Verificador de tautologia
--- data Var  = A | B | C | D | E | True | False
-data Prep = Prep :->: Prep | Prep :&: Prep | Not Prep | A | B | C | D | E | T | F
-type Subs = [(Prep, Bool)]
+-- Verificador de tautologia --
 
--- eval :: Subs -> Prep -> Prep
+-- | define precedencia dos operadores
+infixl 2 :&:
+infixl 4 :->:
+
+-- | Tipo algebrico que representa uma proposição
+data Prop = Prop :->: Prop | Prop :&: Prop | Not Prop | Var Char
+-- | Tipo que representa uma tabela que associa variaveis as suas valoracoes
+type Subs = [(Char, Bool)]
+
+-- | operador infixo que procura uma valor em um Subs (semelhante a lookup)
+infix //
+(//) :: Subs -> Prop -> Bool
+(//) [] _ = False
+(//) (x:xs) (Var var)
+    | fst x == var = snd x
+    | otherwise = xs // (Var var)
+
+-- | avalia o valor de uma proposicao
+eval :: Subs -> Prop -> Bool
+eval subs prop = case prop of
+    v@(Var _)  -> subs // v -- retorna valor de v de acordo com subs
+    Not p      -> not $ eval subs p
+    p1 :&: p2  -> (&&) (eval subs p1) (eval subs p2)
+    p1 :->: p2 -> implic (eval subs p1) (eval subs p2)
+    where
+        implic True False = False
+        implic _    _     = True  
+
+
+-- | retorna o conjunto das variaveis de uma proposicao
+vars :: Prop -> S.Set Char
+vars (Var v) = S.singleton v
+vars (p1 :->: p2) = S.union (vars p1) (vars p2)
+vars (p1 :&: p2) = S.union (vars p1) (vars p2)
+vars (Not p) = S.union (vars p) S.empty
+
+-- | retorna uma lista com todas as valoracoes de tamanho n
+toBin 0 = [0]
+toBin n | n `mod` 2 == 1 = toBin (n `div` 2) ++ [1]
+        | n `mod` 2 == 0 = toBin (n `div` 2) ++ [0]
+
+-- | redimensiona um binario para um array de tamanho n
+resizeBin :: Int -> [Int] -> [Int] 
+resizeBin n bin 
+                | len == n  = bin
+                | len > n   = dropWhile (== 0) bin
+                | otherwise = bin ++ (replicate (n - len) 0)
+                where
+                    len = length bin
+
+-- | bools :: Int -> [[Bool]]
+bools n = map (map intToBool . resizeBin n . toBin) [x | x <- [0.. (2 ^ n - 1)]]
+        where
+            intToBool i = if i == 0 then False else True
+
+-- | retorna todas as possibilidades de valoracao das variaveis de uma Prop
+substs :: Prop -> [Subs]
+substs p = map (zip vs) bs
+        where
+            vs = S.toList . vars $ p
+            bs = bools $ length vs
+
+-- | avalia se uma Prop é tautologia
+-- isTaut :: Prop -> Bool
+isTaut prop = foldr (&&) True $ map (\sub -> eval sub prop) subsList
+            where
+                subsList = substs prop
